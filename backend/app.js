0870d0d8f84a155b46afa0d4faa46839e9b5c49e
Dojando <wadja.b@hotmail.fr>
2020-07-22 19:46:57 +0200

    réorganisation du répertoire de travail et création des routes pour voir toutes les sauces, une sauce, les modifier, créer et supprimer

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce')
 
 mongoose.connect('mongodb+srv://jwd:gZwJSu48qm1vu54K@cluster0.up4ph.mongodb.net/Cluster0?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,6 +22,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
